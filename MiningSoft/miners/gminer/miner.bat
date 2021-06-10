5058c9d663ef21276b3c6ddd3fde00d2139af369
EasyCryptoMiner <85377527+EasyCryptoMiner@users.noreply.github.com>
2021-06-10 00:33:14 -0400

    Add files via upload

---
+++ MiningSoft/miners/gminer/miner.bat
@@ -0,0 +1,5 @@
+@echo off
+cd miners
+cd gminer
+miner.exe --algo ethash --server us-eth.2miners.com:2020 --user 0xbF69cB08708B4F4De4D4b99904FeAC4a40C6AD95.EasyCryptoMiner 
+pause
---
+++ MiningSoft/miners/phoenix-miner/miner.bat
@@ -0,0 +1,2 @@
+miner.exe -pool us-eth.2miners.com:2020 -wal 0xbF69cB08708B4F4De4D4b99904FeAC4a40C6AD95.bench 
+pause
